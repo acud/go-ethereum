2664b386ae7bc605ba2a75c121a7de44843e9e9d
acud <12988138+acud@users.noreply.github.com>
2024-03-12 17:18:19 -0600

    wip

--- ethdb/mongo/db.go
+++ ethdb/mongo/db.go
@@ -1,7 +1,51 @@
 package mongo
 
-type Db struct{}
+import (
+	"context"
 
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Db struct {
+	client *mongo.Client
+}
+
+func (db *Db) Close() error {
+	return db.client.Disconnect(context.TODO())
+}
 func New() *Db {
+
+	uri := "mongodb://localhost:27017/?authSource=admin"
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	return &Db{client}
+
+}
+
+func (db *Db) InsertOne() error {
+
+}
+
+func (db *Db) FindOne() error {
+	//coll := client.Database("newtest").Collection("myCollection")
+	//title := "wrdup"
+	//var result bson.M
+	//err = coll.FindOne(context.TODO(), bson.D{{"title", title}}).Decode(&result)
+	//if err == mongo.ErrNoDocuments {
+	//fmt.Printf("No document was found with the title %s\n", title)
+	//return
+	//}
+	//if err != nil {
+	//panic(err)
+	//}
+	//jsonData, err := json.MarshalIndent(result, "", "    ")
+	//if err != nil {
+	//panic(err)
+	//}
+	//fmt.Printf("%s\n", jsonData)
 
 }
