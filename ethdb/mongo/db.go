28e91c4170155e752a5b1c32d68413d86fdd4a4c
acud <12988138+acud@users.noreply.github.com>
2024-03-13 13:41:17 -0600

    wip

--- ethdb/mongo/db.go
+++ ethdb/mongo/db.go
@@ -2,7 +2,10 @@ package mongo
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -15,7 +18,6 @@ func (db *Db) Close() error {
 	return db.client.Disconnect(context.TODO())
 }
 func New() *Db {
-
 	uri := "mongodb://localhost:27017/?authSource=admin"
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
 	if err != nil {
@@ -23,7 +25,6 @@ func New() *Db {
 	}
 
 	return &Db{client}
-
 }
 
 func (db *Db) InsertOne() error {
@@ -31,21 +32,21 @@ func (db *Db) InsertOne() error {
 }
 
 func (db *Db) FindOne() error {
-	//coll := client.Database("newtest").Collection("myCollection")
-	//title := "wrdup"
-	//var result bson.M
-	//err = coll.FindOne(context.TODO(), bson.D{{"title", title}}).Decode(&result)
-	//if err == mongo.ErrNoDocuments {
-	//fmt.Printf("No document was found with the title %s\n", title)
-	//return
-	//}
-	//if err != nil {
-	//panic(err)
-	//}
-	//jsonData, err := json.MarshalIndent(result, "", "    ")
-	//if err != nil {
-	//panic(err)
-	//}
-	//fmt.Printf("%s\n", jsonData)
+	coll := db.client.Database("newtest").Collection("myCollection")
+	title := "wrdup"
+	var result bson.M
+	err = coll.FindOne(context.TODO(), bson.D{{"title", title}}).Decode(&result)
+	if err == mongo.ErrNoDocuments {
+		fmt.Printf("No document was found with the title %s\n", title)
+		return
+	}
+	if err != nil {
+		panic(err)
+	}
+	jsonData, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", jsonData)
 
 }
